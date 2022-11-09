6d1baa4865ab0350c7da8ff8ff9b124a25519d84
SherinaAB <111399222+SherinaAB@users.noreply.github.com>
2022-11-08 20:49:32 -0500

    Using PATCH

--- public/index.js
+++ public/index.js
@@ -1,10 +1,24 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
 
-    let countValue = 0;
+    let response = await fetch ('http://localhost:9001/counter')
+    //skips promise phase, store value there
+    let obj = await response.json()
+    console.log('obj',obj)
+    let countValue = obj.value;
 
+    fetch ('http://localhost:9001/counter',{
+        method:'PATCH',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "value": 11
+        })
+    })
+    
     function increment(){
         countValue++;
         countContainer.textContent = countValue;
